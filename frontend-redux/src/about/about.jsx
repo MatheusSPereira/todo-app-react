04f5e5008bca917f02b4490177a6be85c4379d69
MatheusSPereira <githubmatheus@outlook.com>
2020-01-13 18:28:09 -0300

    [React-Redux] migration to redux

---
+++ frontend-redux/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageHeader from '../template/pegeHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='About' small='Nós' />
+
+        <h2>Nossa Historia</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
